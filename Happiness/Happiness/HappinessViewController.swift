71f4720652f3a057ef6f4cabb7fd1cd523a20d3e
shihongzhi <shi65881583@gmail.com>
2015-03-22 00:33:38 +0800

    Happiness

---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Hongzhi shi on 15/3/21.
+//  Copyright (c) 2015年 Hongzhi shi. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+    
+}
